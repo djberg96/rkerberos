20b2cd6ea8c7855dd9bfb3d1093828f374679366
Daniel Berger <78529+djberg96@users.noreply.github.com>
2026-01-18 09:22:37 -0500

    Refactor and simplify using pkg-config.

--- ext/rkerberos/extconf.rb
+++ ext/rkerberos/extconf.rb
@@ -1,40 +1,31 @@
+
 require 'mkmf'
 
-# Default paths set based on 4.x installer for Windows.
-if File::ALT_SEPARATOR
-  if ['a'].pack('P').length > 4 # 64-bit
-    dir_config('rkerberos', 'C:/Progra~1/MIT/Kerberos')
-  else
-    dir_config('rkerberos', 'C:/Progra~2/MIT/Kerberos')
-  end
+# Prefer pkg-config for krb5 and dependencies, fallback to manual checks
+if pkg_config('krb5')
+  # pkg_config sets cflags/libs for krb5
 else
-  dir_config('rkerberos', '/usr/local')
-end
-
-have_header('krb5.h')
-
-if File::ALT_SEPARATOR
-  unless have_library('krb5')
+  if File::ALT_SEPARATOR
     if ['a'].pack('P').length > 4 # 64-bit
-      have_library('amd64/krb5_64')
+      dir_config('rkerberos', 'C:/Progra~1/MIT/Kerberos')
     else
-      have_library('i386/krb5_32')
+      dir_config('rkerberos', 'C:/Progra~2/MIT/Kerberos')
     end
+  else
+    dir_config('rkerberos', '/usr/local')
   end
-else
+  have_header('krb5.h')
   have_library('krb5')
 end
 
-unless pkg_config('com_err')
-  puts 'warning: com_err not found, usually a dependency for kadm5clnt'
+pkg_config('com_err') || have_library('com_err')
+
+if pkg_config('kadm5clnt') || have_library('kadm5clnt')
+  have_header('kadm5/admin.h')
 end
 
-if have_header('kadm5/admin.h')
-  have_library('kadm5clnt')
-end
-
-if have_header('kdb.h')
-  have_library('libkdb5')
+if pkg_config('kdb5') || have_library('kdb5')
+  have_header('kdb.h')
 else
   raise 'kdb5 library not found'
 end
