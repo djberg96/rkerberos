e9501f2092bb548f3e5d76a66e5f192b4617b235
Daniel Berger <78529+djberg96@users.noreply.github.com>
2026-02-20 08:36:58 -0500

    Version bump.

--- spec/krb5_spec.rb
+++ spec/krb5_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe Kerberos::Krb5 do
   let(:service) { 'kadmin/admin' }
 
   it 'has the correct version constant' do
-    expect(Kerberos::Krb5::VERSION).to eq('0.2.0')
+    expect(Kerberos::Krb5::VERSION).to eq('0.2.1')
   end
 
   it 'accepts a block and yields itself' do
