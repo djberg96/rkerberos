d737f47ae9bfba03410078d38d5956f01bc3b4fb
Daniel Berger <78529+djberg96@users.noreply.github.com>
2026-03-04 20:04:46 -0500

    Check for profile lib and profile_init_path function.

--- ext/rkerberos/extconf.rb
+++ ext/rkerberos/extconf.rb
@@ -22,10 +22,21 @@ else
       $INCFLAGS << " -I\"#{kfw_inc}\""
       $LDFLAGS << " -L\"#{kfw_lib}\""
 
-      # Try 64-bit first, then 32-bit
+      # headers
       have_header('krb5.h')
-      found_krb5 = have_library('krb5_64') || have_library('krb5_32') || have_library('krb5')
-      found_comerr = have_library('comerr64') || have_library('comerr32') || have_library('com_err')
+      profile_header = have_header('profile.h')
+
+      # libraries needed for base functionality
+      have_library('krb5_64') || have_library('krb5_32') || have_library('krb5')
+      have_library('comerr64') || have_library('comerr32') || have_library('com_err')
+
+      # optional profile support; functions may reside in a separate library
+      if profile_header
+        have_library('profile64') || have_library('profile32') || have_library('profile')
+        have_func('profile_init_path')
+        have_func('profile_release')
+      end
+
       # kadm5clnt and kdb5 are not always present in KfW, so don't fail if missing
       if have_library('kadm5clnt')
         $defs.push("-DHAVE_KADM5_ADMIN_H=1")
@@ -45,6 +56,14 @@ else
       end
       have_header('krb5.h')
       have_library('krb5')
+
+      # profile API may be present separately or baked into krb5
+      profile_header = have_header('profile.h')
+      if profile_header
+        have_library('profile')
+        have_func('profile_init_path')
+        have_func('profile_release')
+      end
 
       # Check for com_err
       if have_pkg_config_lib('com_err')
