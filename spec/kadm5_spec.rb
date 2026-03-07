a42c18b9ec4109eda5efa5d48fbfe94b77009cb8
Daniel Berger <78529+djberg96@users.noreply.github.com>
2026-03-06 21:03:13 -0500

    Update the spec helper, tag specs to be skipped on Windows.

--- spec/kadm5_spec.rb
+++ spec/kadm5_spec.rb
@@ -1,10 +1,10 @@
 # spec/kadm5_spec.rb
 # RSpec tests for Kerberos::Kadm5
-
+require 'spec_helper'
 require 'rkerberos'
 require 'socket'
 
-RSpec.describe 'Kerberos::Kadm5', :kadm do
+RSpec.describe 'Kerberos::Kadm5', :kadm5 do
   subject(:klass){ Kerberos::Kadm5 }
   let(:server){ Kerberos::Kadm5::Config.new.admin_server }
 
@@ -27,34 +27,43 @@ RSpec.describe 'Kerberos::Kadm5', :kadm do
     it 'responds to .new' do
       expect(described_class).to respond_to(:new)
     end
+
     it 'works with valid user and password' do
       expect { described_class.new(principal: user, password: pass) }.not_to raise_error
     end
+
     it 'works with valid service' do
       expect {
         described_class.new(principal: user, password: pass, service: 'kadmin/admin')
       }.not_to raise_error
     end
+
     it 'only accepts a hash argument' do
       expect { described_class.new(user) }.to raise_error(TypeError)
       expect { described_class.new(1) }.to raise_error(TypeError)
     end
+
     it 'accepts a block and yields itself' do
       expect { described_class.new(principal: user, password: pass) {} }.not_to raise_error
       described_class.new(principal: user, password: pass) { |kadm5| expect(kadm5).to be_a(described_class) }
     end
+
     it 'requires principal to be specified' do
       expect { described_class.new({}) }.to raise_error(ArgumentError)
     end
+
     it 'requires principal to be a string' do
       expect { described_class.new(principal: 1) }.to raise_error(TypeError)
     end
+
     it 'requires password to be a string' do
       expect { described_class.new(principal: user, password: 1) }.to raise_error(TypeError)
     end
+
     it 'requires keytab to be a string or boolean' do
       expect { described_class.new(principal: user, keytab: 1) }.to raise_error(TypeError)
     end
+
     it 'requires service to be a string' do
       expect { described_class.new(principal: user, password: pass, service: 1) }.to raise_error(TypeError)
     end
